d1fb03a3407079337de091e9684e87e1f810d9b2
James Loosli <loosli@gmail.com>
2015-07-10 12:58:55 -0700

    taking other projects out of this git repo, cloning them directly in init.sh

--- bin/init.sh
+++ bin/init.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
+# be in the right place.
 cd ~/projects/dotfiles
 
+# copy the basic configs
 for i in vim tmux; do
   rsync -av $i/. ~/
 done
+
+# clone vim stuff direct from github for sanity
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
