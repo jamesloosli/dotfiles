5b3bcfbb6a61aad6cff2d96f44583ffd2412d9d4
James Loosli <loosli@gmail.com>
2015-07-16 15:05:42 -0700

    fixed for loop, tweaked array

--- bin/vim.sh
+++ bin/vim.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 PWD=$(pwd)
-VIM_MODULES_GITHUB=(scrooloose/nerdtree
- fatih/vim-go
- altercation/vim-colors-solarized
- bling/vim-airline
- gkz/vim-ls)
+VIM_MODULES_GITHUB=("scrooloose/nerdtree"\
+ "fatih/vim-go"\
+ "altercation/vim-colors-solarized"\
+ "bling/vim-airline"\
+ "gkz/vim-ls")
 
 # clone vim stuff direct from github for sanity
-for i in $VIM_MODULES_GITHUB; do
+for i in ${VIM_MODULES_GITHUB[@]}; do
   SHORTNAME=$(echo $i | sed 's/.*\///')
-  echo $SHORTNAME
-  if [ ! -d ~/.vim/bundle/$SHORTNAME ]; then
-    git clone https://github.com/$i.git ~/.vim/bundle/$SHORTNAME
-  else
-    cd ~/.vim/bundle/$SHORTNAME && git pull && cd $PWD
-  fi
+  echo "### Updating $SHORTNAME ###"
+  cd ~/.vim/bundle/$SHORTNAME && git pull || git clone https://github.com/$i.git ~/.vim/bundle/$SHORTNAME
+  cd $PWD
 done
 
 cd $PWD
