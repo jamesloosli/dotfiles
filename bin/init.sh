17db0ebb148a61e6ed8b9b35a148493bb8186da8
James Loosli <loosli@gmail.com>
2015-07-16 14:55:02 -0700

    Abstracted vim.sh from init.sh

--- bin/init.sh
+++ bin/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # be in the right place.
 cd ~/Projects/Personal/dotfiles
 
@@ -7,19 +8,6 @@ for i in vim tmux bash; do
   rsync -a $i/. ~/ 
 done
 
-# clone vim stuff direct from github for sanity
-if [ ! -d ~/.vim/bundle/nerdtree ]; then
-  git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-fi
+# kick off the vim.sh setup
+bin/vim.sh
 
-if [ ! -d ~/.vim/bundle/vim-go ]; then
-  git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
-fi
-
-if [ ! -d ~/.vim/bundle/vim-colors-solarized ]; then
-  git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
-fi
-
-if [ ! -d ~/.vim/bundle/vim-airline ]; then
-  git clone git://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
-fi
