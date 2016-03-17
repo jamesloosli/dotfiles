1d0557e91b5639521226f85a1d48c553fe96ef4a
James Loosli <james.loosli@hart.com>
2016-03-17 08:08:02 -0700

    moved my go folder to the Projects directory.

--- bin/go.sh
+++ bin/go.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Set up the Go directories
-mkdir -p ~/Go/src
+mkdir -p ~/Projects/Go/src
 
 # Set the path, and ensure that the path persists
-export GOROOT=$HOME/Go
+export GOROOT=$HOME/Projects/Go
 export PATH=$PATH:$GOROOT/bin
 cp go/bash_go ~/.bash/bash_go
 
